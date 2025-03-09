1c7fd98d263f2b02bac14b49714f68e839cf6701
NIRMAL V G <Nirmalvg97@gmail.com>
2025-03-09 14:38:49 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router"
 import "./index.css"
 import App from "./App.jsx"
 import { Provider } from "react-redux"
-import store from "./app/store.js"
+import store from "../src/app/store.js"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
